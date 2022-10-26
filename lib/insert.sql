82b37dc87932b3a43ef49c7ac2615ccd7c88dfe6
EliusCaleb <eliuswanyoro@gmail.com>
2022-10-26 15:21:59 +0300

    Learning

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (name, age,  sex, color, temperament,alive) VALUES ('Mr Chocolate', 20, 'M','Dark-Brown',' Calm',0);
+
+INSERT INTO bears (name, age,  sex, color, temperament,alive) VALUES ('Rowdy', 0, 'M','Black','Intense',1);
+
+INSERT INTO bears (name, age,  sex, color, temperament,alive) VALUES ('Tabitha', 6, 'F','Dark-brown','Nice',1);
+
+INSERT INTO bears(name, age,  sex, color, temperament,alive) VALUES ('Sergeant Brown', 19, 'M','Green',' Slimy',0);
+
+INSERT INTO bears (name, age,  sex, color, temperament,alive) VALUES ('Melissa', 13, 'F','Dark-Brown',' Goofy',1);
+
+INSERT INTO bears (name, age,  sex, color, temperament,alive) VALUES ('Grinch', 2, 'M','Black','Grinchy',1);
+
+INSERT INTO bears (name, age,  sex, color, temperament,alive) VALUES ('Wendy', 6, 'F','Blue',' Naive',1);
+
+INSERT INTO bears (name, age,  sex, color, temperament,alive) VALUES (NULL, 20, 'M','Black','Aggressive',0);
+
+
